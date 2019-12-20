a12753c0a55ed1be55d442ef0a92e55514d920ee
StellaShi <stella@apples-MacBook-Pro.local>
2019-12-20 18:19:52 +0200

    简化版复合清单

--- products.py
+++ products.py
@@ -10,8 +10,10 @@ while True:
 	if name == 'q': #输入q，退出循环
 		break
 	price = input('请输入商品价格:')
-	p = [] #大清单里的小清单
-	p.append(name)
-	p.append(price)
-	products.append(p) #小清单里有2个元素，把两个元素一起放入大清单
+	p = [name, price] #大清单里的小清单
+	products.append(p) #小清单里有2个元素，把两个元素一起放入大清单（二维清单）
 print(products)
+
+#products[0][0]
+#products这个清单中，第一个集合里面，第一个位置的数据（清单数据第一个为0，第二个为1）
+#products[1][1]，大清单中，第二个位置的第二个数据
